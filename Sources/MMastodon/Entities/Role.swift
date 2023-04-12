61976e7b99708ec9090a86e0814a684f5c421b94
mm <mmdevstuff@gmail.com>
2023-04-12 07:46:23 -0700

    update

--- Sources/MMastodon/Entities/Role.swift
+++ Sources/MMastodon/Entities/Role.swift
@@ -4,18 +4,18 @@ extension MastodonAPI.Entities {
         /// The ID of the Role in the database.
         public var id: String
         /// The name of the role.
-        public var name: String
+        public var name: String?
         /// The hex code assigned to this role. If no hex code is assigned, the string will be empty.
-        public var color: String
+        public var color: String?
         /// An index for the role’s position. The higher the position, the more priority the role has over other roles.
-        public var position: Int
+        public var position: Int?
         /// A bitmask that represents the sum of all permissions granted to the role.
-        public var permissions: RolePermissions
+        public var permissions: RolePermissions?
         /// Whether the role is publicly visible as a badge on user profiles.
-        public var highlighted: Bool
+        public var highlighted: Bool?
         /// The date that the role was created. ISO 8601 Datetime.
-        public var created_at: String
+        public var created_at: String?
         /// The date that the role was updated.
-        public var updated_at: String
+        public var updated_at: String?
     }
 }
